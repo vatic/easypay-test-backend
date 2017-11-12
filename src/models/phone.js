ab5f02aefdb83aeb909490308ccaa77a65073589
Sergey Vatagin <s.vatagin@gmail.com>
2017-11-12 14:47:00 +0300

    refactor verifyPhone

--- src/models/phone.js
+++ src/models/phone.js
@@ -2,16 +2,13 @@ const knex = require('../db/knex');
 
 const tableName = 'phones';
 
-/*
-  @param - phone - string, XXX-XXX-XXXXX or XXX.XXX.XXXX or XXX XXX XXXX
-*/
-const verifyPhone = (phone) => {
-  const phoneMatcher = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-  if (phone.match(phoneMatcher)) {
-    return true;
-  }
-  return false;
-};
+/**
+ * Verify phone number.
+ * @param  {string} phone - XXX-XXX-XXXXX or XXX.XXX.XXXX or XXX XXX XXXX
+ */
+const verifyPhone = phone =>
+  /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(phone);
+
 
 const listPhones = () =>
   knex(tableName).select('*').limit(10);
