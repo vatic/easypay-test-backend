97ab171d1d7944f9f917e713c59d13e5f8731c84
Sergey Vatagin <s.vatagin@gmail.com>
2017-11-12 14:23:29 +0300

    add verify phone

--- src/models/phone.js
+++ src/models/phone.js
@@ -2,12 +2,27 @@ const knex = require('../db/knex');
 
 const tableName = 'phones';
 
+/*
+  @param - phone - string, XXX-XXX-XXXXX or XXX.XXX.XXXX or XXX XXX XXXX
+*/
+const verifyPhone = (phone) => {
+  const phoneMatcher = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+  if (phone.match(phoneMatcher)) {
+    return true;
+  }
+  return false;
+};
+
 const listPhones = () =>
   knex(tableName).select('*').limit(10);
 
+const addPhone = phone =>
+  knex(tableName).insert(phone).limit(10);
+
 module.exports = {
   listPhones,
-  // addPhone,
+  addPhone,
   // removePhone,
   // checkPhone,
-}
+  verifyPhone,
+};
